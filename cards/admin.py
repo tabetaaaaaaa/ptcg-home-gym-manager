cfb8b7879af5e0dca3a7e5b000814ba304a9df54
tabetaaaaaaa <tabetaaaaaaa7@gmail.com>
2025-12-03 22:24:31 +0900

    Installation of the `django-import-export` library

--- cards/admin.py
+++ cards/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin # 追加
 from .models import PokemonCard, Type, EvolutionStage, SpecialFeature, MoveType
 
-admin.site.register(PokemonCard)
+# PokemonCardAdminを定義し、ImportExportModelAdminを継承させる
+class PokemonCardAdmin(ImportExportModelAdmin):
+    pass
+
+admin.site.register(PokemonCard, PokemonCardAdmin) # 変更
 admin.site.register(Type)
 admin.site.register(EvolutionStage)
 admin.site.register(SpecialFeature)
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export', # django-import-export の追加
     'cards.apps.CardsConfig',
 ]
 
