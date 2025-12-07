d6a444789a786de29d6234a64054f396146e6222
tabetaaaaaaa <tabetaaaaaaa7@gmail.com>
2025-12-07 16:25:07 +0900

    エラー解消、チェックボックスによる登録機能の導入

--- cards/forms.py
+++ cards/forms.py
@@ -6,13 +6,13 @@ class PokemonCardForm(forms.ModelForm):
         model = PokemonCard
         fields = '__all__'
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
-            'quantity': forms.NumberInput(attrs={'class': 'input input-bordered w-full'}),
+            'name': forms.TextInput(attrs={'class': 'input input-bordered input-primary w-full'}),
+            'quantity': forms.NumberInput(attrs={'class': 'input input-bordered input-primary w-full'}),
             'evolution_stage': forms.Select(attrs={'class': 'select select-bordered w-full'}),
-            'evolves_from': forms.Select(attrs={'class': 'select select-bordered w-full'}),
-            'types': forms.SelectMultiple(attrs={'class': 'select select-bordered w-full'}),
-            'special_features': forms.SelectMultiple(attrs={'class': 'select select-bordered w-full'}),
-            'move_types': forms.SelectMultiple(attrs={'class': 'select select-bordered w-full'}),
-            'memo': forms.Textarea(attrs={'class': 'textarea textarea-bordered w-full', 'rows': 3}),
-            'image': forms.ClearableFileInput(attrs={'class': 'file-input file-input-bordered w-full'}),
+            'evolves_from': forms.TextInput(attrs={'class': 'input input-bordered w-full', 'placeholder': '例: ピカチュウ'}),
+            'types': forms.CheckboxSelectMultiple(attrs={'class': 'checkbox checkbox-primary'}),
+            'special_features': forms.CheckboxSelectMultiple(attrs={'class': 'checkbox checkbox-secondary'}),
+            'move_types': forms.CheckboxSelectMultiple(attrs={'class': 'checkbox checkbox-accent'}),
+            'memo': forms.Textarea(attrs={'class': 'textarea textarea-bordered textarea-primary w-full', 'rows': 3}),
+            'image': forms.ClearableFileInput(attrs={'class': 'file-input file-input-bordered file-input-primary w-full'}),
         }
