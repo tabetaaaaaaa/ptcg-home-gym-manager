ddf1ea71d73aacfca0b9a3f6e3fe7f20011c28ad
tabetaaaaaaa <tabetaaaaaaa7@gmail.com>
2025-12-07 14:05:37 +0900

    新規登録・編集フォームの改善

--- cards/forms.py
+++ cards/forms.py
@@ -6,13 +6,13 @@ class PokemonCardForm(forms.ModelForm):
         model = PokemonCard
         fields = '__all__'
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
-            'quantity': forms.NumberInput(attrs={'class': 'input input-bordered w-full'}),
-            'evolution_stage': forms.Select(attrs={'class': 'select select-bordered w-full'}),
-            'evolves_from': forms.Select(attrs={'class': 'select select-bordered w-full'}),
-            'types': forms.SelectMultiple(attrs={'class': 'select select-bordered w-full'}),
-            'special_features': forms.SelectMultiple(attrs={'class': 'select select-bordered w-full'}),
-            'move_types': forms.SelectMultiple(attrs={'class': 'select select-bordered w-full'}),
-            'memo': forms.Textarea(attrs={'class': 'textarea textarea-bordered w-full', 'rows': 3}),
-            'image': forms.ClearableFileInput(attrs={'class': 'file-input file-input-bordered w-full'}),
+            'name': forms.TextInput(),
+            'quantity': forms.NumberInput(),
+            'evolution_stage': forms.Select(),
+            'evolves_from': forms.TextInput(),
+            'types': forms.CheckboxSelectMultiple(),
+            'special_features': forms.CheckboxSelectMultiple(),
+            'move_types': forms.CheckboxSelectMultiple(),
+            'memo': forms.Textarea(attrs={'rows': 3}),
+            'image': forms.ClearableFileInput(),
         }
