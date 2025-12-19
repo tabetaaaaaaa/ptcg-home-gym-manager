4c69bc92194d1d70ed20b24b58a5daa2a9467f9d
tabetaaaaaaa <tabetaaaaaaa7@gmail.com>
2025-12-19 20:29:34 +0900

    追加フィールドの閲覧・検索実装完了。検索UI改善中

--- cards/filters.py
+++ cards/filters.py
@@ -35,11 +35,39 @@ class PokemonCardFilter(django_filters.FilterSet):
         widget=forms.CheckboxSelectMultiple
     )
 
+    weakness = django_filters.ModelMultipleChoiceFilter(
+        queryset=Type.objects.all(),
+        label='弱点',
+        widget=forms.CheckboxSelectMultiple
+    )
+
+    resistance = django_filters.ModelMultipleChoiceFilter(
+        queryset=Type.objects.all(),
+        label='抵抗力',
+        widget=forms.CheckboxSelectMultiple
+    )
+
+    hp = django_filters.RangeFilter(
+        field_name='hp',
+        label='HP',
+        widget=django_filters.widgets.RangeWidget(attrs={'class': 'input input-bordered input-sm w-full'})
+    )
+
+    retreat_cost = django_filters.RangeFilter(
+        field_name='retreat_cost',
+        label='にげる',
+        widget=django_filters.widgets.RangeWidget(attrs={'class': 'input input-bordered input-sm w-full'})
+    )
+
     CHOICES = (
         ('name', '名前 (昇順)'),
         ('-name', '名前 (降順)'),
         ('quantity', '枚数 (少ない順)'),
         ('-quantity', '枚数 (多い順)'),
+        ('hp', 'HP (少ない順)'),
+        ('-hp', 'HP (多い順)'),
+        ('retreat_cost', 'にげる (少ない順)'),
+        ('-retreat_cost', 'にげる (多い順)'),
         ('evolution_stage__display_order', '進化度合い (昇順)'),
         ('-evolution_stage__display_order', '進化度合い (降順)'),
         ('-created_at', '登録日時 (新しい順)'),
@@ -51,6 +79,8 @@ class PokemonCardFilter(django_filters.FilterSet):
         fields=(
             'name',
             'quantity',
+            'hp',
+            'retreat_cost',
             'evolution_stage__display_order',
             'created_at',
         ),
@@ -60,7 +90,7 @@ class PokemonCardFilter(django_filters.FilterSet):
     class Meta:
         model = PokemonCard
         # fields の指定により、GETパラメータのキー名とフィルタのフィールド名が一致する
-        fields = ['name', 'types', 'evolution_stage', 'special_features', 'move_types', 'ordering']
+        fields = ['name', 'types', 'evolution_stage', 'special_features', 'move_types', 'weakness', 'resistance', 'hp', 'retreat_cost', 'ordering']
 
 
 class TrainersCardFilter(django_filters.FilterSet):
